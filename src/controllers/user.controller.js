53cca8966af7e9f275c4d333fb1fa09d606621c7
yambeni <yambenimirriam@gmail.com>
2024-01-16 10:09:57 +0200

    delete & update users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import * as argon from 'argon2';
+import Note from '../models/Note.js';
 import User from '../models/User.js';
 
 /******* CREATE NEW USER ******/
@@ -61,10 +62,72 @@ export const getAllUsers = async (req, res) => {
 
 /******* UPDATE USER ******/
 export const updateUser = async (req, res) => {
-  return res.status(200).json({ message: 'update user' });
+  const id = req.params.id;
+  const { username, roles, active, password } = req.body;
+
+  // Does the user exist to update?
+  const user = await User.findById(id).exec();
+
+  if (!user) {
+    return res.status(400).json({ message: 'User not found' });
+  }
+
+  // Check for duplicate
+  const duplicate = await User.findOne({ username })
+    .collation({ locale: 'en', strength: 2 })
+    .lean()
+    .exec();
+
+  // Allow updates to the original user
+  if (duplicate && duplicate?._id.toString() !== id) {
+    return res.status(409).json({ message: 'Duplicate username' });
+  }
+
+  user.username = username;
+  user.roles = roles;
+  user.active = active;
+
+  if (password) {
+    // Hash password
+    user.password = await argon.hash(password);
+  }
+
+  const updatedUser = await user.save();
+  // If user update fails, return a bad request response
+  if (!updatedUser) {
+    return res.status(400).json({
+      success: false,
+      message: 'Invalid user data received',
+    });
+  }
+
+  return res.status(200).json({ message: 'updated user' });
 };
 
 /******* DELETE ALL USER ******/
 export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+
+  // Does the user still have assigned notes?
+  const note = await Note.findOne({ userId: id }).lean().exec();
+  if (note) {
+    return res.status(400).json({ message: 'User has assigned notes' });
+  }
+
+  // Does the user exist to delete?
+  const user = await User.findById(id).exec();
+
+  if (!user) {
+    return res.status(400).json({ message: 'User not found' });
+  }
+
+  const result = await user.deleteOne();
+  // If user deletion fails, return a bad request response
+  if (!result) {
+    return res.status(400).json({
+      success: false,
+      message: 'Error occured',
+    });
+  }
   return res.status(200).json({ message: 'delete user' });
 };
