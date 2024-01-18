347143529a27660f8275665ba1d5ab2e0afe0fa7
yambeni <yambenimirriam@gmail.com>
2024-01-18 21:11:02 +0200

    create & get notes

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -1,11 +1,48 @@
+import Note from '../models/Note.js';
+import User from '../models/User.js';
+
 /******* CREATE NEW NOTE *******/
 export const createNote = async (req, res) => {
+  const { title, text } = req.body;
+
+  // Check for duplicate title
+  const note = await Note.findOne({ title })
+    .collation({ locale: 'en', strength: 2 })
+    .lean()
+    .exec();
+
+  if (note) {
+    return res.status(409).json({ message: 'Note title already exists' });
+  }
+
+  // Create and store the new user
+  const newNote = await Note.create({ userId: req.user.id, title, text });
+
+  if (!newNote) {
+    return res.status(400).json({ message: 'Invalid note data received' });
+  }
+
   return res.status(201).json({ message: 'New note created' });
 };
 
 /******* GET ALL NOTES *******/
 export const getAllNotes = async (req, res) => {
-  return res.status(200).json({ message: 'Notes' });
+  // Get all notes from MongoDB
+  const notes = await Note.find().lean();
+
+  // If no notes
+  if (!notes?.length) {
+    return res.status(400).json({ message: 'No notes found' });
+  }
+
+  const notesWithUser = await Promise.all(
+    notes.map(async (note) => {
+      const user = await User.findById(note.userId).lean().exec();
+      return { ...note, username: user.username };
+    })
+  );
+
+  return res.status(200).json(notesWithUser);
 };
 
 /******* UPDATE NOTE *******/
